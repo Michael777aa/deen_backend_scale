5ff6b9b6b2c82072c48695ce9642b831bbded546
Michael777aa <shoxabdufattoyev485@gmail.com>
2024-11-23 12:55:50 +0900

    fix: modify erors

--- src/public/js/coupon.js
+++ src/public/js/coupon.js
@@ -1,4 +1,3 @@
-console.log(`Coupon expires on: ${couponExpiry}`);
 function generateCouponName() {
   const randomName = "Coupon-" + Math.random().toString(36).substring(2, 8);
   document.getElementById("name").value = randomName;
