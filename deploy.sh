757ba23e91c2ae4da90159f9e9a65520ff528e4f
Mike <mikemike@Mikeui-MacBookPro.local>
2025-12-10 19:01:29 +0900

    feat: develop

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,7 @@
-#!/bin/bash
-
-# PRODUCTION
-
 git reset --hard
 git checkout master
 git pull origin master
 
-npm i 
-npm run build 
-pm2 start process.config.js --env production
-
-
-#DEVELOPMENT
-# git rest --hard 
-# git checkout develop
-# git pull origin develop
-
-
-# npm i
-# pm2 start "npm run start:prod" --name=LMS
+npm install
+npm run build
+npm run start:prod
